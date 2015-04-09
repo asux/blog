3449ccbd0e4259355138b8fcc7f49fa9e6b506a4
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-09 23:57:00 +0300

    Debugging AWS credentials

--- bin/setup-eb.sh
+++ bin/setup-eb.sh
@@ -10,11 +10,17 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOF
 fi
 
+# show AWS credentials
+if [ ! -z $DEBUG ]; then
+  echo "AWS credentials:"
+  cat ~/.aws/credentials
+fi
+
 # setup EB CLI
 eb --version || (sudo pip install awsebcli && eb --version)
 [ -d .elasticbeanstalk ] || mkdir -p .elasticbeanstalk
 if [ ! -f .elasticbeanstalk/config.yml ]; then
-  eb_init_cmd="eb init '$EB_APP' --profile 'default' --region '$AWS_REGION' --platform '$EB_PLATFORM'"
+  eb_init_cmd="eb init '$EB_APP' --region '$AWS_REGION' --platform '$EB_PLATFORM'"
   echo $eb_init_cmd
   $eb_init_cmd
 fi
