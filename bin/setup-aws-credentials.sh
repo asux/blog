f6c40dacb89e5fe42da328fa4635fd4d1f762a2a
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-10 02:00:59 +0300

    Removed showing AWS credentials

--- bin/setup-aws-credentials.sh
+++ bin/setup-aws-credentials.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 if [ ! -f ~/.aws/credentials ]; then
+  echo "Creating ~/.aws/credentials"
   mkdir -p ~/.aws
   cat <<-EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOF
-fi
-
-# show AWS credentials
-if [ ! -z $DEBUG ]; then
-  echo "AWS credentials:"
-  cat ~/.aws/credentials
 fi
