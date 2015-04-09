0d33ea955e2f303fa1264e6e90fc31d4da3c8cfe
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-10 00:11:18 +0300

    Fixing setup-eb.sh

--- bin/setup-eb.sh
+++ bin/setup-eb.sh
@@ -18,11 +18,10 @@ fi
 
 # setup EB CLI
 eb --version || (sudo pip install awsebcli && eb --version)
-[ -d .elasticbeanstalk ] || mkdir -p .elasticbeanstalk
 if [ ! -f .elasticbeanstalk/config.yml ]; then
   eb_init_cmd="eb init '$EB_APP' --region '$AWS_REGION' --platform '$EB_PLATFORM'"
-  echo $eb_init_cmd
-  $eb_init_cmd
+  echo "$eb_init_cmd"
+  bash -c "${eb_init_cmd}"
 fi
 
 # show EB env status
